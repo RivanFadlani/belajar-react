169647d6e5e587b3a14fa9559db68a5818a327eb
RivanFadlani <rivanfadlani7@gmail.com>
2026-06-17 15:57:30 +0700

    study: Props (170626)
    Mempelajari fungsi dari Props, cara mengirim dan menerimanya,
    implementasi Destructuring Parameter pada Props, implementasi Spread
    Syntax di Parent Component

--- belajar-react-dasar/src/hello-world/HelloWorld.jsx
+++ belajar-react-dasar/src/hello-world/HelloWorld.jsx
@@ -1,24 +1,33 @@
+// === Parent Component = Mengirim informasi ke Child Component menggunakan Props
 export function HelloWorld() {
+  // object props di sini menampung properties yang nantinya akan dipakai sebagai attr di child component
+  const props = {
+
+    subTitle: "Text Apa Ya",
+    text: "Semangat belajarnya Ripunn!"
+  }
   return (
     <div>
-      <HeaderHelloWorld />
-      <ParagraphHelloWorld />
+      {/* mengirim attribute text ke HeaderHelloWorld() dengan value Hello World */}
+      <HeaderHelloWorld text="Hello World" />
+      {/* spread syntax sangat berguna saat kita ingin forward semua properties yang ada di object */}
+      <ParagraphHelloWorld {...props} />
     </div>
   )
 }
 
-function HeaderHelloWorld() {
-  const text = "Hello World"
-  // untuk mengakses sebuah variable harus menggunakan kurung kurawal '{}'
-  // khusus untuk style, harus menggunakan double kurung kurawal, karena sebenarnya dia sedang memanggil javascript object untuk styling
-  // dan, seluruh attribute value wajib menggunakan kurung kurawal. contohnya <img src={...}>
-  return <h1 style={{ color: "white", backgroundColor: "royalblue" }}>{text.toUpperCase()}</h1>
+// === Child Component
+// props itu seperti mirip dengan attr di <html>
+// bedanya, kita bisa mengirim nilai javascript. seperti object, array, function, dll.
+function HeaderHelloWorld(props) {
+  // memanggil parameter props dengan {props.text}
+  return <h1 style={{ color: "white", backgroundColor: "royalblue" }}>{props.text.toUpperCase()}</h1>
 }
 
-function ParagraphHelloWorld() {
-  const text = "Semangat belajarnya Ripunn!"
-  // Yang dimaksud double kurung kurawal di attribute style
-  // variable isinya object, otomatis kalau ingin inline (styling langsung di tag html), kita harus menggunakan double kurung kurawal
+// Destructuring Props langsung di parameter function (Destructuring Parameter pada Props)
+// ini memudahkan kita untuk tahu attr apa yang tersedia pada sebuah component
+// kita juga bisa membuat default value jika props text tidak ada value
+function ParagraphHelloWorld({ text = "Ga ada textnya loh ya!", subTitle }) {
   const style = {
     color: "royalblue",
     backgroundColor: "pink",
@@ -26,6 +35,7 @@ function ParagraphHelloWorld() {
   }
   return (
     <div>
+      <h2>{subTitle}</h2>
       <p style={style}>{text.toLowerCase()}</p>
       <hr />
     </div>
