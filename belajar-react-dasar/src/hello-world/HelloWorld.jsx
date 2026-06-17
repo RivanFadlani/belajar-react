b9f571a273920004e4d4f95891df3f7116308ff8
RivanFadlani <rivanfadlani7@gmail.com>
2026-06-17 11:46:50 +0700

    JavaScript di JSX (170626)

--- belajar-react-dasar/src/hello-world/HelloWorld.jsx
+++ belajar-react-dasar/src/hello-world/HelloWorld.jsx
@@ -1,5 +1,3 @@
-// dalam satu file, kita bisa membuat lebih dari satu component (multiple component)
-
 export function HelloWorld() {
   return (
     <div>
@@ -10,13 +8,25 @@ export function HelloWorld() {
 }
 
 function HeaderHelloWorld() {
-  return <h1>Hello World</h1> // kalau hanya satu line saja, kita bisa lakukan tanpa '()'
+  const text = "Hello World"
+  // untuk mengakses sebuah variable harus menggunakan kurung kurawal '{}'
+  // khusus untuk style, harus menggunakan double kurung kurawal, karena sebenarnya dia sedang memanggil javascript object untuk styling
+  // dan, seluruh attribute value wajib menggunakan kurung kurawal. contohnya <img src={...}>
+  return <h1 style={{ color: "white", backgroundColor: "royalblue" }}>{text.toUpperCase()}</h1>
 }
 
 function ParagraphHelloWorld() {
+  const text = "Semangat belajarnya Ripunn!"
+  // Yang dimaksud double kurung kurawal di attribute style
+  // variable isinya object, otomatis kalau ingin inline (styling langsung di tag html), kita harus menggunakan double kurung kurawal
+  const style = {
+    color: "royalblue",
+    backgroundColor: "pink",
+    fontWeight: "bold"
+  }
   return (
     <div>
-      <p>Semangat belajarnya Ripunn!</p>
+      <p style={style}>{text.toLowerCase()}</p>
       <hr />
     </div>
   )
