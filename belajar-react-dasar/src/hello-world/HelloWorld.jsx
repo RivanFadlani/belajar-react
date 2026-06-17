e296369d91862ecbabf4a17cac9926111be0b369
RivanFadlani <rivanfadlani7@gmail.com>
2026-06-17 18:02:47 +0700

    study: Style
    Mempelajari bagaimana caranya mengambil nama class dari CSS ke Component

--- belajar-react-dasar/src/hello-world/HelloWorld.jsx
+++ belajar-react-dasar/src/hello-world/HelloWorld.jsx
@@ -1,6 +1,6 @@
-// === Parent Component = Mengirim informasi ke Child Component menggunakan Props
+import "./HelloWorld.css"
+
 export function HelloWorld() {
-  // object props di sini menampung properties yang nantinya akan dipakai sebagai attr di child component
   const props = {
 
     subTitle: "Text Apa Ya",
@@ -8,35 +8,25 @@ export function HelloWorld() {
   }
   return (
     <div>
-      {/* mengirim attribute text ke HeaderHelloWorld() dengan value Hello World */}
       <HeaderHelloWorld text="Hello World" />
-      {/* spread syntax sangat berguna saat kita ingin forward semua properties yang ada di object */}
       <ParagraphHelloWorld {...props} />
     </div>
   )
 }
 
-// === Child Component
-// props itu seperti mirip dengan attr di <html>
-// bedanya, kita bisa mengirim nilai javascript. seperti object, array, function, dll.
 function HeaderHelloWorld(props) {
-  // memanggil parameter props dengan {props.text}
-  return <h1 style={{ color: "white", backgroundColor: "royalblue" }}>{props.text.toUpperCase()}</h1>
+  // memanggil nama class dengan attr className
+  // title
+  return <h1 className="title">{props.text.toUpperCase()}</h1>
 }
 
-// Destructuring Props langsung di parameter function (Destructuring Parameter pada Props)
-// ini memudahkan kita untuk tahu attr apa yang tersedia pada sebuah component
-// kita juga bisa membuat default value jika props text tidak ada value
 function ParagraphHelloWorld({ text = "Ga ada textnya loh ya!", subTitle }) {
-  const style = {
-    color: "royalblue",
-    backgroundColor: "pink",
-    fontWeight: "bold"
-  }
+
   return (
     <div>
       <h2>{subTitle}</h2>
-      <p style={style}>{text.toLowerCase()}</p>
+      {/* content */}
+      <p className="content">{text.toLowerCase()}</p>
       <hr />
     </div>
   )
