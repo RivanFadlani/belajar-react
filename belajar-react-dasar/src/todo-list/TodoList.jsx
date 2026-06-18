93fadb14c2a12cf8b91d84e744d6204419eb0250
RivanFadlani <rivanfadlani7@gmail.com>
2026-06-18 09:41:20 +0700

    study: Collection Component
    Penggunaan array.map() untuk mengubah data array manjadi Component

--- belajar-react-dasar/src/todo-list/TodoList.jsx
+++ belajar-react-dasar/src/todo-list/TodoList.jsx
@@ -3,16 +3,44 @@ import Todo from "./Todo"
 // Parent Component dari 'Todo.jsx'
 // Menerima import dari Child, dan mengirim Prop ke Child Component
 export default function TodoList() {
+  // Membuat data array yang isinya object
+  const data = [
+    {
+      // object id untuk id unique
+      id: 1,
+      text: "Learn HTML",
+      isCompleted: true
+    },
+    {
+      id: 2,
+      text: "Learn CSS",
+      isCompleted: true
+    },
+    {
+      id: 3,
+      text: "Learn JavaScript",
+      isCompleted: true
+    },
+    {
+      id: 4,
+      text: "Learn React",
+      isCompleted: false
+    },
+  ]
+
+
   return (
     <ul>
-      {/* Kondisi isCompleted = true */}
-      {/* Component Todo memiliki tag 'li' */}
-      <Todo isCompleted={true} text="Learn HTML" />
-      {/* isDeleted = true */}
-      <Todo isCompleted={true} text="Learn CSS" isDeleted={true} />
-      <Todo isCompleted={true} text="Learn JavaScript" />
-      {/* Kondisi isCompleted = false */}
-      <Todo isCompleted={false} text="Learn React" />
+      {/* gunakan array.map() untuk data array menjadi Component */}
+      {/* gunakan spread untuk menangkap semua object yang ada di array */}
+
+      {/* kalau tidak memakai spread, maka akan seperti ini: */}
+      {/* <Todo text={todo.text} isCompleted={todo.isCompleted} /> */}
+
+      {/* Setiap Component perlu id unique menggunakan attr 'key' */}
+      {data.map((todo) => (
+        < Todo key={todo.id} {...todo} />)
+      )}
     </ul>
   )
 }
