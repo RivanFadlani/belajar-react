02d351e87a06ac079e55befd2d06a0297269503e
RivanFadlani <rivanfadlani7@gmail.com>
2026-06-22 13:51:23 +0700

    study: Reset State p1 (220626)
    Cara pertama adalah dengan cara mengubah component ke component yang
    lain

--- belajar-react-dasar/src/counter/CounterApp.jsx
+++ belajar-react-dasar/src/counter/CounterApp.jsx
@@ -10,35 +10,9 @@ export function CounterApp() {
 
   return (
     <div>
-      {/* ----------------------------------------------------------------------
-        RENDER KONDISIONAL: OPERATOR TERNARY (? :) & KONSEP "POSISI YANG SAMA"
-        ----------------------------------------------------------------------
-        Sintaks dari eksperimen Anda: {show ? <Counter name="ripunn" /> : <Counter name="rivan" />}
-        
-        MENGAPA KETIKA 'SHOW' BERUBAH, STATE COUNTER TIDAK IKUT RESET?
-        
-        - Hanya Ada 1 Slot Posisi: 
-          Bagi React, satu operator ternary hanya dihitung sebagai SATU slot posisi 
-          tetap di Render Tree.
-        
-        - Hanya Ada 1 Instance (Wujud Fisik):
-          React tidak pernah membuat 2 komponen lalu menghancurkan salah satunya. 
-          Sejak awal, React hanya menghidupkan 1 instance di slot tersebut.
-        
-        - Hanya Mengupdate Props:
-          Saat 'show' berubah dari true ke false, React melihat tipe komponennya masih 
-          sama-sama <Counter />. React memutuskan untuk menggunakan kembali (re-use) 
-          instance fisik yang ada, dan HANYA mengubah datanya (props) dari 
-          name="ripunn" menjadi name="rivan".
-           
-        - Dampak pada State:
-           Karena komponennya tidak pernah dihancurkan (unmounted), maka STATE internal 
-           di dalam <Counter /> (seperti angka hitungan) TETAP DIPERTAHANKAN.
-
-        INTINYA: - jadi intinya react hanya merender ulang propsnya saja, tidak dengan state. 
-                   karena component counter tidak pernah benar-benar hilang dari tampilan. 
-      */}
-      {show ? <Counter name="ripunn" /> : <Counter name="rivan" />}
+      {/* Cara Reset State P.1 */}
+      {/* Mengubah dengan Component yang lain: <Counter /> -> <p /> */}
+      {show ? <Counter name="ripunn" /> : <p>Menghilang</p>}
       <input type="checkbox" checked={show} onChange={handleChange} />
     </div>
   )
