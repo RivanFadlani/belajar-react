11c1980ca1d52570cd4707bffeebedbbe0a457b9
RivanFadlani <rivanfadlani7@gmail.com>
2026-06-22 14:03:02 +0700

    study: Reset State p2 (220626)
    cara kedua adalah dengan cara mengubah posisi component (mengubah
    struktur UI)

--- belajar-react-dasar/src/counter/CounterApp.jsx
+++ belajar-react-dasar/src/counter/CounterApp.jsx
@@ -10,9 +10,26 @@ export function CounterApp() {
 
   return (
     <div>
-      {/* Cara Reset State P.1 */}
-      {/* Mengubah dengan Component yang lain: <Counter /> -> <p /> */}
-      {show ? <Counter name="ripunn" /> : <p>Menghilang</p>}
+      {/* Cara Reset State P.2 */}
+      {/* ===
+      Mengubah posisi component. Karena React menyimpan State sesuai dengan
+      Component dan Posisinya (struktur UI).
+
+      Struktur UI yang dimaksud adalah:
+      - pada awalnya Component Counter disimpan di posisi 'div -> Counter'
+      - lalu, karena kondisi yang ke dua merubah posisi Component Counter menjadi 
+        'section -> counter', maka otomatis react akan menganggap Component Counter
+        sudah dipindahkan dan State pun akan di-reset
+      === */}
+      {show ? (
+        <div>
+          <Counter name="ripunn" />
+        </div>
+      ) : (
+        <section>
+          <Counter name="rivan" />
+        </section>
+      )}
       <input type="checkbox" checked={show} onChange={handleChange} />
     </div>
   )
