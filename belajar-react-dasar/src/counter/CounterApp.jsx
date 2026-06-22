10dd4cc25d97f511cb85e3dd4f0a19afe52a0b9c
RivanFadlani <rivanfadlani7@gmail.com>
2026-06-22 16:04:12 +0700

    study: Reset State p4 (220626)
    cara ketiga adalah dengan cara menggunakan key pada component
    (direkomendasikan)

--- belajar-react-dasar/src/counter/CounterApp.jsx
+++ belajar-react-dasar/src/counter/CounterApp.jsx
@@ -12,22 +12,12 @@ export function CounterApp() {
     <div>
       {/* Cara Reset State P.3 */}
       {/* ===
-      POSISI COMPONENT BERBEDA (Dua Slot Terpisah di Render Tree)
+      Menggunakan Key (Paling direkomendasikan)
+
+      Meskipun menggunakan ternary dan di dalam posisi yang sama
       === */}
 
-      {/* {...} bukan Component, melainkan wadah/ruang untuk menjalankan logika JavaScript */}
-      {/* dalam kasus ini adalah 'show && </>' */}
-      {/* disebut beda posisi karena kita membuat dua baris perintah independen
-          lalu, karena ada 2 kurung kurawal juga membuat React melihatnya sebagai 
-          dua koordinat/slot yang berbeda di dalam Render Tree */}
-
-      {/* Component Counter 1 (Menempati Slot Posisi A / Baris 1) */}
-      {/* Jika 'show' true -> Instance ini dibuat. Jika false -> Instance ini BENAR-BENAR DIHANCURKAN (State Hilang) */}
-      {show && <Counter name="ripunn" />}
-
-      {/* Component Counter 2 (Menempati Slot Posisi B / Baris 2) */}
-      {/* Jika 'show' false -> Instance ini dibuat dari nol dengan State baru (0) */}
-      {!show && <Counter name="rivan" />}
+      {show ? <Counter key="ripunn" name="ripunn" /> : <Counter key="rivan" name="rivan" />}
 
       <input type="checkbox" checked={show} onChange={handleChange} />
     </div>
