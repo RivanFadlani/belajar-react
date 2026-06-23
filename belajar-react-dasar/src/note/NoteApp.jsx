28f95998e5d17c5780180950bd2d66ce578c2ec9
RivanFadlani <rivanfadlani7@gmail.com>
2026-06-23 10:25:54 +0700

    study: Reducer p3 (230626)
    Membuat component dengan banyak state menggunakan useImmerReduce untuk
    mempermudah akses object

--- belajar-react-dasar/src/note/NoteApp.jsx
+++ belajar-react-dasar/src/note/NoteApp.jsx
@@ -5,9 +5,9 @@
  * `use-immer` mempermudah pembaruan state bertipe array/object kompleks dengan cara mutasi langsung pada draf bayangan (draft state).
  */
 
+import { useImmerReducer } from "use-immer"
 import NoteForm from "./NoteForm"
 import NoteList from "./NoteList"
-import { useReducer } from "react"
 
 // Variabel auto-increment untuk menghasilkan ID unik pada setiap catatan baru
 let id = 0
@@ -20,30 +20,28 @@ const initialNotes = [
   { id: id++, text: "Learn React", done: false },
 ]
 
-// notes = state saat ini
+// draft = state saat ini dari useImmerReducer
 // action = object yang dikirim dari dispatch yang tertrigger oleh component 
-function notesReducer(notes, action) {
-  switch (action.type) {
-    case "ADD_NOTE":
-      return [
-        ...notes,
-        {
-          id: id++,
-          text: action.text,
-          done: false
-        }
-      ]
-    case "CHANGE_NOTE":
-      return notes.map((note) => note.id === action.id ? { ...note, text: action.text, done: action.done } : note)
-    case "DELETE_NOTE":
-      return notes.filter((note) => (note.id !== action.id))
-    default:
-      return notes
+function notesReducer(draft, action) {
+  if (action.type === "ADD_NOTE") {
+    draft.push({
+      id: id++,
+      text: action.text,
+      done: false
+    });
+  } else if (action.type === "CHANGE_NOTE") {
+    const index = draft.findIndex((note) => note.id === action.id)
+    draft[index].text = action.text
+    draft[index].done = action.done
+  } else if (action.type === "DELETE_NOTE") {
+    const index = draft.findIndex((note) => note.id === action.id)
+    // splice, hapus dari index ke berapa dan berapa elemen dari index awal
+    draft.splice(index, 1)
   }
 }
 
 export default function NoteApp() {
-  const [notes, dispatch] = useReducer(notesReducer, initialNotes)
+  const [notes, dispatch] = useImmerReducer(notesReducer, initialNotes)
 
   const handleAddNote = (text) => {
     // dispatch adalah jembatan untuk mengirim Object (action) dari Component menuju ke reducer.
