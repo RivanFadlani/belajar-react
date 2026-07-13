8cf6e512d17f1128fd690cb8775855dcd5841084
RivanFadlani <rivanfadlani7@gmail.com>
2026-07-13 21:15:33 +0700

    study(ch.2): Use-Navigate

--- belajar-react-router/src/pages/Home.tsx
+++ belajar-react-router/src/pages/Home.tsx
@@ -1,8 +1,20 @@
+import { useNavigate } from "react-router";
+
 const Home = () => {
+  // Navigasi antar page menggunakan function javascript
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate({
+      pathname: "/data",
+    });
+  };
+
   return (
     <div>
       <h1>Selamat Datang di Home</h1>
       <p>Kamu masuk route (/)</p>
+      <button onClick={handleClick}>Go To Data Page</button>
     </div>
   );
 };
