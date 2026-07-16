dab4fb42cb6f0c24f457be75b050b089e0fdf0c9
RivanFadlani <rivanfadlani7@gmail.com>
2026-07-16 15:35:37 +0700

    feat(store, route): create store and route

--- tugas/tugas_todolist-redux/src/App.tsx
+++ tugas/tugas_todolist-redux/src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <>
-      <h1 className="text-red-500">Hello World</h1>
+      <h1 className="text-red-500">Hello World!</h1>
     </>
   );
 };
--- tugas/tugas_todolist-redux/src/main.tsx
+++ tugas/tugas_todolist-redux/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store.ts";
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
-)
+);
